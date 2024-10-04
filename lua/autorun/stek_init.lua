f5b89cc846f35666f7004edfcbcab9ffe1915108
sekta <akovlevmihail535@gmail.com>
2024-10-04 07:48:05 +0400

    Initial

---
+++ lua/autorun/stek_init.lua
@@ -0,0 +1,32 @@
+stek_run = {}
+
+if SERVER then
+    AddCSLuaFile("stek/init.lua")
+
+    function stek_run.server(path)
+        return include(path)
+    end
+
+    function stek_run.shared(path)
+        AddCSLuaFile(path)
+
+        return include(path)
+    end
+
+    function stek_run.client(path)
+        AddCSLuaFile(path)
+    end
+else
+    function stek_run.server()
+    end
+
+    function stek_run.shared(path)
+        return include(path)
+    end
+
+    function stek_run.client(path)
+        return include(path)
+    end
+end
+
+include("stek/init.lua")
---
+++ lua/stek/init.lua
@@ -0,0 +1,1 @@
+print("Sometimes, i dream about cheese...")
