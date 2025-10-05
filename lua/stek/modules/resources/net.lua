bdc8a74167af67a95d17f665f73408804cdb93ea
sekta2 <akovlevmihail535@gmail.com>
2025-10-06 03:24:34 +0400

    📝 Написал комментарии + аннотации для ресурсов

--- lua/stek/modules/resources/net.lua
+++ lua/stek/modules/resources/net.lua
@@ -1,3 +1,5 @@
+---Записывает в пакет uid ресурса, если ресурс не найден, возвращает ошибку
+---@param res_resolvable Resource|string|number
 function net.WriteSTEKResource(res_resolvable)
     local res = type(res_resolvable) == "table" and res_resolvable or (stek.Resources.index[res_resolvable] or stek.Resources.list[res_resolvable])
     if not res then
@@ -7,6 +9,7 @@ function net.WriteSTEKResource(res_resolvable)
     net.WriteUInt(res.uid, stek.Resources.bits_count)
 end
 
+---Читает и возвращает ресурс из пакета
 function net.ReadSTEKResource()
     local uid = net.ReadUInt(stek.Resources.bits_count)
     return stek.Resources.list[uid]
--- lua/stek/modules/resources/sh.lua
+++ lua/stek/modules/resources/sh.lua
@@ -1,6 +1,28 @@
+---@class ResourceAutoEntity
+
+---
+
+---@class ResourceData
+---@field name string? Название ресурса, используется когда не найдено переведённое название
+---@field icon string? Путь к значку ресурса, если нет, то используется стандартный путь(см. :GetIconPath())
+---@field volume number? Объём ресурса
+---@field auto_entity boolean? Нужно ли автоматически создавать энтити для этого ресурса?
+---@field entity ResourceAutoEntity? Параметры энтити ресурса
+
+---
+
+---@class Resource: ResourceData
+---@field uid number Уникальный Идентификатор ресурса
+---@field id string Идентификатор ресурса
+---@field volume number Объём ресурса
+---@field auto_entity boolean Нужно ли автоматически создавать энтити для этого ресурса?
 local ResourceClass = {}
 ResourceClass.__index = ResourceClass
 
+---Создаёт и возвращает объект ресурса
+---@param id string Идентификатор ресурса
+---@param data ResourceData Параметры ресурса
+---@return Resource
 function ResourceClass:new(id, data)
     data = data or {}
 
@@ -19,6 +41,8 @@ function ResourceClass:new(id, data)
     return setmetatable(Object, self)
 end
 
+---Возвращает название ресурса
+---@return string
 function ResourceClass:GetName()
     local phrase_name = "resource_" .. self.id
     if not stek.Locale.Exists(phrase_name) then return self.name or self.id end
@@ -26,10 +50,14 @@ function ResourceClass:GetName()
     return stek.Locale.Get("resource_" .. self.id)
 end
 
+---Возвращает путь к значку ресурса
+---@return string
 function ResourceClass:GetIconPath()
     return self.icon or ("materials/stek/resources/" .. self.id .. ".png")
 end
 
+---Возвращает материал значка ресурса, если путь не валидный, возвращает nil
+---@return IMaterial?
 function ResourceClass:GetIcon()
     if self.icon_mat then
         return self.icon_mat
@@ -46,6 +74,12 @@ end
 
 ---
 
+---# Ресурсный модуль
+---Позволяет создавать ресурсы
+---@class ResourceModule
+---@field list table<number, Resource>
+---@field index table<string, Resource>
+---@field bits_count number
 local Resources = {
     list = {},
     index = {},
@@ -53,6 +87,10 @@ local Resources = {
     bits_count = 1
 }
 
+---Создаёт новый ресурс и возвращает его
+---@param id string Идентификатор ресурса
+---@param data ResourceData Параметры ресурса
+---@return Resource
 function Resources.Add(id, data)
     if not id or (id and type(id) ~= "string") then error(("invalid resource id '%s'"):format("id")) end
 
@@ -69,14 +107,16 @@ function Resources.Add(id, data)
     return Object
 end
 
-Resources.Add("test", {
-
-})
-
+---Возвращает ресурс по указанному UID
+---@param uid number Уникальный Идентификатор ресурса
+---@return Resource
 function Resources.GetByUID(uid)
     return Resources.list[uid]
 end
 
+---Возвращает ресурс по указанному идентификатору
+---@param id string Идентификатор ресурса
+---@return Resource
 function Resources.GetByID(id)
     return Resources.index[id]
 end
