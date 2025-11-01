5896b4211a04dff8fd1d43c83fcd448145ca6053
sekta2 <akovlevmihail535@gmail.com>
2025-11-01 07:21:47 +0400

    feat(pipe): plugging

--- lua/entities/ent_stek_pipe.lua
+++ lua/entities/ent_stek_pipe.lua
@@ -2,6 +2,7 @@ AddCSLuaFile()
 
 ---@diagnostic disable: assign-type-mismatch
 ---@class ent_stek_pipe: Entity
+---@field ConnectorPlug ent_stek_plug
 ENT = ENT
 
 ENT.Base = "base_anim"
@@ -29,6 +30,43 @@ if SERVER then
 
     ---@param activator Player
     function ENT:Use(activator)
+        if not activator:KeyDown(IN_WALK) then return end
+
+        local connector = self.ConnectorPlug
+        if connector and IsValid(connector) then
+            connector:SetPos(activator:GetEyeTraceNoCursor().HitPos)
+            activator:PickupObject(connector)
+
+            return
+        end
+
+        local plug = stek.MakeConnector(self, activator:GetEyeTraceNoCursor().HitPos)
+        plug.TouchCallback = function(_, TouchEntity, Plug)
+            if not (TouchEntity:GetClass() == "ent_stek_pipe" and TouchEntity ~= self) then return end
+
+            Plug:Remove()
+
+            timer.Simple(0, function()
+                local constr, rope = constraint.Rope(
+                    self, -- Entity 1
+                    TouchEntity, -- Entity 2
+                    0, -- Bone 1
+                    0, -- Bone 2
+                    vector_origin, -- LocalPos 1
+                    vector_origin, -- LocalPos 2
+                    500, -- Length
+                    0, -- Add length
+                    2500, -- Force Limit
+                    15, -- Width
+                    "cable/cable2" -- Material
+                )
+            end)
+        end
+
+        activator:DropObject()
+	    activator:PickupObject(plug)
+
+        self.ConnectorPlug = plug
     end
 
     function ENT:OnRemove()
