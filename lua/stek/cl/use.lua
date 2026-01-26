a641dd90251168d4f9bf00fabb615be28aefa840
sekta2 <akovlevmihail535@gmail.com>
2026-01-26 18:14:30 +0400

    feat: add client-side (Entity:Use) method

---
+++ lua/stek/cl/use.lua
@@ -0,0 +1,22 @@
+---@class Entity
+---@field ClientUse fun(ply:Player) Called when an local player uses this entity. (CLIENT)
+
+local USE_DISTANCE = 10000
+local USE_BIND = "+use"
+
+hook.Add("PlayerBindPress", "stek.ClientUse", function(ply, bind, pressed)
+    if pressed and bind == USE_BIND then
+        local tr = ply:GetEyeTrace()
+
+        if IsValid(tr.Entity) then
+            local dist = ply:GetPos():DistToSqr(tr.HitPos)
+            local fn_client_use = tr.Entity.ClientUse
+
+            if dist < USE_DISTANCE and fn_client_use then
+                fn_client_use(LocalPlayer())
+
+                return false
+            end
+        end
+    end
+end)
