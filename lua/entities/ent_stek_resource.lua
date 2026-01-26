8bbc332e1f2d20f49fdffd7179053d718e388a74
sekta2 <akovlevmihail535@gmail.com>
2026-01-26 15:32:04 +0400

    chore: move resource sync logic

--- lua/entities/ent_stek_resource.lua
+++ lua/entities/ent_stek_resource.lua
@@ -23,39 +23,6 @@ function ENT:GetAmount()
 end
 
 if SERVER then
-    local function GetDatafiedResources()
-        local all_resources = {}
-
-        for ent_index, _ in pairs(stek.Resources.active_ents) do
-            ---@type ent_stek_resource
-            local ent = Entity(ent_index)
-            if not (IsValid(ent) and ent:GetAmount() ~= 100) then continue end
-
-            all_resources[#all_resources + 1] = { ent_index, ent:GetAmount() }
-        end
-
-        if #all_resources <= 0 then return false end
-
-        local data = util.Compress(util.TableToJSON(all_resources))
-
-        return data, #data
-    end
-
-    util.AddNetworkString("stek.SyncResourceAmount")
-    util.AddNetworkString("stek.SyncResourceAmountFull")
-
-    hook.Add("PlayerAuthed", "stek.Resources.SyncResoucesAmount", function(ply)
-        local data, len = GetDatafiedResources()
-        if not (data and len) then return end
-
-        net.Start("stek.SyncResourceAmountFull")
-        net.WriteUInt(len, 16)
-        net.WriteData(data, len)
-        net.Send(ply)
-    end)
-
-    ---
-
     function ENT:Initialize()
         if self.Model then
             self:SetModel(self.Model)
@@ -112,6 +79,7 @@ if SERVER then
     end
 else
     net.Receive("stek.SyncResourceAmount", function()
+        ---@type ent_stek_resource
         local resource_ent = net.ReadEntity()
         if not IsValid(resource_ent) then return end
 
@@ -137,6 +105,7 @@ else
         for i = 1, #tbl do
             local data = tbl[i]
 
+            ---@type ent_stek_resource
             local ent = Entity(data[1])
             if not IsValid(ent) then continue end
 
--- lua/stek/modules/resources/sv.lua
+++ lua/stek/modules/resources/sv.lua
@@ -1,3 +1,36 @@
+---@diagnostic disable: assign-type-mismatch
+
+local function GetDatafiedResources()
+    local all_resources = {}
+
+    for ent_index, _ in pairs(stek.Resources.active_ents) do
+        ---@type ent_stek_resource
+        local ent = Entity(ent_index)
+        if not (IsValid(ent) and ent:GetAmount() ~= 100) then continue end
+
+        all_resources[#all_resources + 1] = { ent_index, ent:GetAmount() }
+    end
+
+    if #all_resources <= 0 then return false end
+
+    local data = util.Compress(util.TableToJSON(all_resources))
+
+    return data, #data
+end
+
+util.AddNetworkString("stek.SyncResourceAmount")
+util.AddNetworkString("stek.SyncResourceAmountFull")
+
+hook.Add("PlayerAuthed", "stek.Resources.SyncResoucesAmount", function(ply)
+    local data, len = GetDatafiedResources()
+    if not (data and len) then return end
+
+    net.Start("stek.SyncResourceAmountFull")
+    net.WriteUInt(len, 16)
+    net.WriteData(data, len)
+    net.Send(ply)
+end)
+
 ---@param ent ent_stek_resource
 ---@param ply Player
 hook.Add("GetPreferredCarryAngles", "stek.Resource.CarryAngles", function(ent, ply)
