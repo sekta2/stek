b97da48ce6d274e696c44b2606883cd449d2cece
sekta2 <akovlevmihail535@gmail.com>
2026-01-26 13:16:47 +0400

    feat: add craft module

---
+++ lua/stek/modules/craft/sh.lua
@@ -0,0 +1,25 @@
+local Craft = {
+    list = {},
+    index = {},
+
+    bits_count = 1
+}
+
+function Craft.Create(id, data)
+    local uid = #Craft.list + 1
+
+    Craft.list[uid] = data
+    Craft.index[id] = data
+
+    Craft.bits_count = stek.BitsForUnsignedInt(uid)
+end
+
+function Craft.GetByUID(uid)
+    return Craft.list[uid]
+end
+
+function Craft.GetByID(id)
+    return Craft.index[id]
+end
+
+stek.Craft = Craft
