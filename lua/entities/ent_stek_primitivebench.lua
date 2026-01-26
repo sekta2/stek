5c7787652e853bd1a04f52d3142772fef0313fd0
sekta2 <akovlevmihail535@gmail.com>
2026-01-26 15:40:35 +0400

    feat: add crafting table entity and workbench entity

---
+++ lua/entities/ent_stek_primitivebench.lua
@@ -0,0 +1,31 @@
+AddCSLuaFile()
+
+---@diagnostic disable: assign-type-mismatch
+---@class ent_stek_primitivebench: Entity
+ENT = ENT
+
+ENT.Base = "base_anim"
+ENT.Type = "anim"
+
+ENT.PrintName = "Crafting Table"
+ENT.Category = "STek: Machines"
+ENT.Author = "sekta"
+ENT.Spawnable = true
+
+if SERVER then
+    function ENT:Initialize()
+        self:SetModel("models/hunter/blocks/cube1x2x1.mdl")
+
+        self:SetMoveType(MOVETYPE_VPHYSICS)
+        self:SetUseType(SIMPLE_USE)
+        self:SetSolid(SOLID_VPHYSICS)
+
+        self:PhysicsInit(SOLID_VPHYSICS)
+        self:SetUseType(SIMPLE_USE)
+        self:PhysWake()
+    end
+else
+    function ENT:Draw()
+        self:DrawModel()
+    end
+end
