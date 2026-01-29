13add7696f0c0fefc5dfb55f61502c6a44441c3f
sekta2 <akovlevmihail535@gmail.com>
2026-01-29 21:51:23 +0400

    chore: fix types

--- lua/stek/modules/craft/net.lua
+++ lua/stek/modules/craft/net.lua
@@ -1,16 +1,17 @@
 ---Записывает в пакет uid крафта, если крафт не найден, возвращает ошибку
----@param res_resolvable Resource|string|number
-function net.WriteSTEKCraft(res_resolvable)
-    local craft = type(res_resolvable) == "table" and res_resolvable or
-        (stek.Craft.index[res_resolvable] or stek.Craft.list[res_resolvable])
+---@param craft_resolvable Craft|string|number
+function net.WriteSTEKCraft(craft_resolvable)
+    local craft = type(craft_resolvable) == "table" and craft_resolvable or
+        (stek.Craft.index[craft_resolvable] or stek.Craft.list[craft_resolvable])
     if not craft then
-        error(("Unknown craft '%s'"):format(res_resolvable))
+        error(("Unknown craft '%s'"):format(craft_resolvable))
     end
 
     net.WriteUInt(craft.uid, stek.Craft.bits_count)
 end
 
 ---Читает и возвращает крафт из пакета
+---@return Craft
 function net.ReadSTEKCraft()
     local uid = net.ReadUInt(stek.Craft.bits_count)
     return stek.Craft.list[uid]
