89c99f8be6a2ef9eae1e3cd4fad793055ab6db7d
sekta2 <akovlevmihail535@gmail.com>
2026-07-16 00:51:51 +0400

    fix: client use

--- lua/stek/cl/use.lua
+++ lua/stek/cl/use.lua
@@ -1,5 +1,5 @@
 ---@class Entity
----@field ClientUse fun(ply:Player) Called when an local player uses this entity. (CLIENT)
+---@field ClientUse fun(self: Entity, ply:Player) Called when an local player uses this entity. (CLIENT)
 
 local USE_DISTANCE = 10000
 local USE_BIND = "+use"
@@ -13,7 +13,7 @@ hook.Add("PlayerBindPress", "stek.ClientUse", function(ply, bind, pressed)
             local fn_client_use = tr.Entity.ClientUse
 
             if dist < USE_DISTANCE and fn_client_use then
-                fn_client_use(ply)
+                fn_client_use(tr.Entity, ply)
 
                 return false
             end
