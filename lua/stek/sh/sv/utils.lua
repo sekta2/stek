c5acb4c05d7dabd0ed89e5a7a8f22bcc51222ccf
FumoFumo022 <73023316+nnk000@users.noreply.github.com>
2026-07-19 23:16:21 +0300

    feat: Create server utils.lua, add explosion function

---
+++ lua/stek/sh/sv/utils.lua
@@ -0,0 +1,58 @@
+
+--[[---------------------------------------------------------
+
+	stek.CreateExplosion( vector_origin, angle_zero, ply, 128,
+	 256, 32 + 64, ply, 6144, DMG_BURN, ply, prop_physics )
+
+	Creates CEnvExplosion entity with a lot of options
+
+	inflictor = ent that "created" an explosion ( grenade )
+
+	attacker = entity that caused an explosion creation
+	( player that pulled the pin  )
+
+-----------------------------------------------------------]]
+
+function stek.CreateExplosion( pos, ang, attacker, magnitude, radius, flags, inflictor, force, dmgType, ignore, ignoreClass )
+
+	local explosion = ents.Create( "env_explosion" )
+
+	explosion:SetPos( pos )
+	explosion:SetAngles( ang )
+	explosion:SetParent( inflictor )
+
+	explosion:SetKeyValue( "iMagnitude", tostring( magnitude ) )
+
+	if flags then
+		explosion:AddSpawnFlags( flags )
+	end
+
+	if radius then
+		explosion:SetKeyValue( "iRadiusOverride", tostring( radius ) )
+	end
+
+	if force ~= 0 then
+		explosion:SetKeyValue( "DamageForce", tostring( force ) )
+	end
+
+	explosion:SetSaveValue( "m_hInflictor", inflictor )
+
+	if ignore then
+		explosion:SetSaveValue( "m_hEntityIgnore", ignore )
+	end
+
+	if ignoreClass then
+		explosion:SetSaveValue( "m_iClassIgnore", ignoreClass )
+	end
+
+	if dmgType then
+		explosion:SetSaveValue( "m_iCustomDamageType", dmgType )
+	end
+
+	explosion:SetRenderMode( RENDERMODE_TRANSADD )
+	explosion:SetOwner( attacker )
+	explosion:Spawn()
+
+	explosion:Fire( "Explode", 0, 0 )
+
+end
