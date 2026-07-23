dddd7f46402471a621764355d047dd3e78ed3c21
sekta2 <akovlevmihail535@gmail.com>
2026-07-23 22:05:01 +0400

    feat(gas-system): add collisions

--- lua/stek/modules/gas-system/sv.lua
+++ lua/stek/modules/gas-system/sv.lua
@@ -81,6 +81,8 @@ end
 function GasSystem.Update()
     if not GasSystem.pull then return end
 
+    local to_collision_update = {}
+
     GasSystem.pull:Foreach(function(cell)
         ---@type GasParticle
         local Particle = cell.data
@@ -103,7 +105,6 @@ function GasSystem.Update()
         if trace.StartSolid then
             local newpos = find_free_pos(Particle.pos, Vector(-10, -10, -10), Vector(10, 10, 10))
             Particle.pos = newpos
-            Particle.velocity = vector_origin
         elseif trace.Hit then
             Particle.pos = trace.HitPos + trace.HitNormal * 1
 
@@ -114,9 +115,79 @@ function GasSystem.Update()
             Particle.pos = trace.HitPos
         end
 
+        local pos = Particle.pos
+        local shg_lastpos = Particle.shg_lastpos
+        local new_shgpos = Vector(math.ceil(pos.x / SHG_SIZE), math.ceil(pos.y / SHG_SIZE), math.ceil(pos.z / SHG_SIZE))
+
+        if new_shgpos ~= shg_lastpos then
+            GasSystem.shg:RemoveObject(shg_lastpos.x, shg_lastpos.y, shg_lastpos.z, Particle)
+            GasSystem.shg:AddObject(new_shgpos.x, new_shgpos.y, new_shgpos.z, Particle)
+
+            Particle.shg_lastpos = new_shgpos
+        end
+
         Particle.nextupdate = CurTime() + math.Rand(1, 1.5)
         Particle.lastupdatetime = CurTime()
+
+        to_collision_update[#to_collision_update+1] = Particle
     end)
+
+    for i = 1, #to_collision_update do
+        ---@type GasParticle
+        local Particle = to_collision_update[i]
+        local shg_pos = Particle.shg_lastpos
+
+        for x = -1, 1 do
+            for y = -1, 1 do
+                for z = -1, 1 do
+                    local shgcell = GasSystem.shg:GetCell(shg_pos.x + x, shg_pos.y + y, shg_pos.z + z)
+                    if shgcell then
+                        for i = 1, #shgcell do
+                            ---@type GasParticle
+                            local other_par = shgcell[i]
+                            if other_par == Particle then goto skip end
+
+                            local collision_axis = Particle.pos - other_par.pos
+                            local dist = collision_axis:Length()
+                            local min_dist = 20 + 20
+
+                            if dist < min_dist then
+                                local n
+                                if dist < 0.0001 then
+                                    n = VectorRand():GetNormalized()
+                                else
+                                    n = collision_axis / dist
+                                end
+
+                                local delta = min_dist - dist
+
+                                local trace1 = util.TraceHull({
+                                    start = Particle.pos,
+                                    endpos = Particle.pos + 0.5 * delta * n,
+                                    mins = Vector(-10, -10, -10),
+                                    maxs = Vector(10, 10, 10),
+                                    mask = bit.bor(MASK_SOLID, MASK_WATER)
+                                })
+
+                                local trace2 = util.TraceHull({
+                                    start = other_par.pos,
+                                    endpos = other_par.pos - 0.5 * delta * n,
+                                    mins = Vector(-10, -10, -10),
+                                    maxs = Vector(10, 10, 10),
+                                    mask = bit.bor(MASK_SOLID, MASK_WATER)
+                                })
+
+                                Particle.pos = trace1.HitPos
+                                other_par.pos = trace2.HitPos
+                            end
+
+                            ::skip::
+                        end
+                    end
+                end
+            end
+        end
+    end
 
     GasSystem.SyncAll()
 end
